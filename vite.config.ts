944c644f43ee8c8fd594403151fb2fdbacd21c07
Izaias Lima <izaiaslima356@gmail.com>
2025-02-08 09:05:18 -0300

    feat: remove test environment

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import tsconfigPaths from "vite-tsconfig-paths";
-import { defineConfig } from "vitest/config";
-
-export default defineConfig({
-	plugins: [tsconfigPaths()],
-	test: {
-		environmentMatchGlobs: [["src/http/controllers/**", "prisma"]], // name prisma must like the name after vitest-environment- folder. For example, if the folder name is vitest-environment-prisma, then the name must be prisma.
-	},
-});
